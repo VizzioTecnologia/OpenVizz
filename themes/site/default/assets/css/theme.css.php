1469117c9ec06c37a6afa97e1e10616475fe85eb
Frank Souza <franksouza183@gmail.com>
2016-05-01 19:42:10 -0300

    default site theme updated

--- themes/site/default/assets/css/theme.css.php
+++ themes/site/default/assets/css/theme.css.php
@@ -1,16 +1,13 @@
-<?php if(extension_loaded('zlib')){ob_start('ob_gzhandler');} header ('Content-Type: text/css');
+<?php if ( extension_loaded( 'zlib' ) ){ ob_start( 'ob_gzhandler' ); } header ( 'Content-Type: text/css' );
 
 require_once 'theme.header.css.php';
 
 require_once( BASE_PATH . 'application/config/constants.php');
 require_once( BASE_PATH . 'application/config/config.php');
 
-
 define('THEME_IMAGE_DIR_URL', SITE_THEMES_URL . '/' . $template . '/assets/images');
 define('THEME_IMAGE_DIR_PATH', SITE_THEMES_PATH . $template . DS . 'assets' . DS . 'images' . DS );
 define('THEME_FONTS_DIR_URL', SITE_THEMES_URL . '/' . $template . '/assets/fonts');
-
-
 
 /*
  **************************************************************************************************
@@ -27,7 +24,7 @@ $vui = new Vui(
 	
 	THEME_IMAGE_DIR_PATH . 'svg' . DS,
 	THEME_IMAGE_DIR_PATH . 'svg' . DS . 'colors.svg'
-	//THEME_IMAGE_DIR_URL . '/svg'
+	//THEME_IMAGE_DIR_URL . '/svg' // uncomment to cache svg images
 	
 );
 /*
@@ -40,12 +37,13 @@ $vui = new Vui(
 
 include( 'defs.php' );
 include( 'reset.css' );
-include( 'fonts.css' );
+// include( 'fonts.css' );
 include( 'font_icons.css' );
 include( 'html.css' );
 
 include( 'inputs.css' );
 include( 'buttons.css' );
+include( 'mimes.css' );
 
 if ( isset( $_GET[ 'fb' ] ) AND $_GET[ 'fb' ] == 1 ) {
 	
--- themes/site/default/index.php
+++ themes/site/default/index.php
@@ -7,19 +7,27 @@
 			
 			$this->plugins->load( array( 'names' => array( 'jquery', 'jquery_scrolltop' ), 'types' => array( 'js_tooltip' ) ) );
 			
-			$this->voutput->append_head_stylesheet( 'theme', SITE_THEMES_URL . '/' . site_theme() . '/assets/css/theme.css.php' );
-			$this->voutput->append_head_stylesheet( 'google_font', 'http://fonts.googleapis.com/css?family=Droid+Sans+Mono|Roboto:400,100,100italic,300,300italic,400italic,700,700italic|Roboto+Condensed:300italic,400italic,700italic,400,300,700' );
+			if ( $this->voutput->get_head_stylesheet( 'fancybox' ) ) {
+				
+				$this->voutput->unset_head_stylesheet( 'fancybox' );
+				
+				$get_args[] = 'fb=1';
+				
+			}
+			
+			$this->voutput->append_head_stylesheet( 'theme', SITE_THEMES_URL . '/' . site_theme() . '/assets/css/theme.css.php' . ( ! empty( $get_args ) ? '?' . join( '&', $get_args ) : '' ) );
+			$this->voutput->append_head_stylesheet( 'google_font', 'https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,300italic,700|Open+Sans:400,300,300italic,400italic,700,700italic' );
 			$this->voutput->append_head_script( 'theme', SITE_THEMES_URL . '/' . site_theme() . '/assets/js/functions.js' );
 			
 			$google_font_script = "
 				
 				WebFontConfig = {
-					google: { families: [ 'Droid+Sans+Mono::latin', 'Roboto:400,100,100italic,300,300italic,400italic,700,700italic:latin', 'Roboto+Condensed:300italic,400italic,700italic,400,300,700:latin' ] }
+					google: { families: [ 'Open+Sans+Condensed:300,300italic,700:latin', 'Open+Sans:400,300,300italic,400italic,700,700italic:latin' ] }
 				};
 				(function() {
 					var wf = document.createElement('script');
 					wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
-						'://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+					'://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
 					wf.type = 'text/javascript';
 					wf.async = 'true';
 					var s = document.getElementsByTagName('script')[0];
@@ -32,11 +40,17 @@
 			
 			echo $this->voutput->get_head();
 			
+			$content_disp = '';
+			
+			if ( check_var( $this->mcm->loaded_modules[ 'left' ] ) ) $content_disp .= 'l';
+			if ( trim( $this->voutput->get_content() ) !== '' ) $content_disp .= 'c';
+			if ( check_var( $this->mcm->loaded_modules[ 'right' ] ) ) $content_disp .= 'r';
+			
 		?>
 		
 	</head>
 	
-	<body class="vui <?= $this->ua->is_mobile() ? 'mobile' : 'desktop'; ?> <?= ( @$this->mcm->loaded_modules[ 'top_banner' ] ) ? 'with-top-banner' : ''; ?>">
+	<body class="vui content-disp-<?= $content_disp; ?> <?= $this->ua->is_mobile() ? 'mobile' : 'desktop'; ?> <?= ( check_var( $this->mcm->loaded_modules[ 'top_banner' ] ) ) ? 'with-top-banner' : ''; ?>">
 		
 		<?= $this->voutput->get_body_start(); ?>
 		
@@ -48,7 +62,7 @@
 					
 					<div id="top-logo">
 						
-						<?php if ( @$this->mcm->loaded_modules[ 'logo' ] ){ ?>
+						<?php if ( check_var( $this->mcm->loaded_modules[ 'logo' ] ) ){ ?>
 						
 						<?php foreach ( @$this->mcm->loaded_modules[ 'logo' ] as $key => $module ) { echo @$module; } ?>
 						
@@ -60,17 +74,17 @@
 						
 					</div>
 					
-					<?php if ( @$this->mcm->loaded_modules[ 'top-other-info' ] ){ ?>
+					<?php if ( check_var( $this->mcm->loaded_modules[ 'top_other_info' ] ) ){ ?>
 						
 						<div id="top-other-info">
 							
-							<?php foreach ( @$this->mcm->loaded_modules[ 'top-other-info' ] as $key => $module ) { echo @$module; } ?>
+							<?php foreach ( @$this->mcm->loaded_modules[ 'top_other_info' ] as $key => $module ) { echo @$module; } ?>
 							
 						</div>
 						
 					<?php } ?>
 					
-					<?php if ( @$this->mcm->loaded_modules[ 'top_menu' ] ){ ?>
+					<?php if ( check_var( $this->mcm->loaded_modules[ 'top_menu' ] ) ){ ?>
 					<div id="top-menu">
 						
 						<?php foreach ( @$this->mcm->loaded_modules[ 'top_menu' ] as $key => $module ) {
@@ -86,7 +100,7 @@
 				
 			</div>
 			
-			<?php if ( @$this->mcm->loaded_modules[ 'top_banner' ] ){ ?>
+			<?php if ( check_var( $this->mcm->loaded_modules[ 'top_banner' ] ) ){ ?>
 			
 			<div id="top-banner-block">
 				
@@ -108,7 +122,7 @@
 			
 			<?php } ?>
 			
-			<?php if ( @$this->mcm->loaded_modules[ 'after_banner' ] ){ ?>
+			<?php if ( check_var( $this->mcm->loaded_modules[ 'after_banner' ] ) ){ ?>
 			
 			<div id="after-banner-block">
 				
@@ -132,87 +146,169 @@
 			
 			<?php } ?>
 			
-			<?php if ( isset( $msg ) AND ! empty( $msg ) AND $msg !== '' ){ ?>
-			
-			<div id="msg-block">
-				
-				<div class="s1">
-					
-					<div class="s2">
-						
-						<?= $msg; ?>
-						
-					</div>
-					
-				</div>
-				
-				<div class="shadow"></div>
-				
-			</div>
-			
-			<?php } ?>
-			
-			<?php if ( trim( $this->voutput->get_content() ) !== '' ){ ?>
-			
 			<div id="content-block">
 				
 				<div class="s1">
 					
-					<div class="s2">
+					<?php if ( check_var( $this->mcm->loaded_modules[ 'left' ] ) ){ ?>
+						
+						<div id="left-modules" class="s2 ">
+							
+							<div id="left-logo" class="module-wrapper">
+							
+								<a title="<?= strip_tags( $this->mcm->filtered_system_params[ 'site_name' ] ); ?>" class="logo" href="<?= site_url(); ?>"><span class="site-name"><?= $this->mcm->filtered_system_params[ 'site_name' ]; ?></span></a>
+								
+							</div><?php
+							
+							foreach ( $this->mcm->loaded_modules[ 'left' ] as $key => $module ) {
+								
+								echo @$module;
+								
+							} ?>
+							
+						</div><?php 
+						
+					}
+					
+					if ( check_var( $this->mcm->loaded_modules[ 'right' ] ) ){
+						
+						?><div id="right-modules" class="s2 ">
+							
+							<?php foreach ( $this->mcm->loaded_modules[ 'right' ] as $key => $module ) { ?>
+								
+								<?= @$module; ?>
+								
+							<?php } ?>
+							
+						</div><?php
+						
+					}
+					
+					?><div id="content" class="s2">
+						
+						<?php if ( check_var( $this->mcm->loaded_modules[ 'ad_top_content' ] ) ){ ?>
+							
+							<div id="ad-top-content-modules" class="s2 ">
+								
+								<?php foreach ( $this->mcm->loaded_modules[ 'ad_top_content' ] as $key => $module ) { ?>
+									
+									<?= @$module; ?>
+									
+								<?php } ?>
+								
+							</div>
+							
+						<?php } ?>
+						
+						<?php if ( isset( $msg ) AND ! empty( $msg ) AND $msg !== '' ){ ?>
+							
+							<div id="msg-block">
+								
+								<div class="s1">
+									
+									<div class="s2">
+										
+										<?= $msg; ?>
+										
+									</div>
+									
+								</div>
+								
+								<div class="shadow"></div>
+								
+							</div>
+							
+						<?php } ?>
+						
+						<?php if ( check_var( $this->mcm->loaded_modules[ 'top_content' ] ) ){ ?>
+							
+							<div id="top-content-modules" class="s2 ">
+								
+								<?php foreach ( $this->mcm->loaded_modules[ 'top_content' ] as $key => $module ) { ?>
+									
+									<?= @$module; ?>
+									
+								<?php } ?>
+								
+							</div>
+							
+						<?php } ?>
 						
 						<?= $this->voutput->get_content(); ?>
 						
+						<?php if ( check_var( $this->mcm->loaded_modules[ 'bottom_content' ] ) ){ ?>
+						
+						<div id="bottom-content-block">
+							
+							<div class="s1">
+								
+								<div class="s2">
+									
+									<?php foreach ( $this->mcm->loaded_modules[ 'bottom_content' ] as $key => $module ) {
+										
+										echo @$module;
+										
+									} ?>
+									
+								</div>
+								
+							</div>
+							
+						</div>
+						
+						<?php } ?>
+						
 					</div>
 					
 				</div>
 				
 			</div>
-			
-			<?php } ?>
-			
-			<?php if ( @$this->mcm->loaded_modules[ 'bottom_content' ] ){ ?>
-			
-			<div id="bottom-content-block">
-				
-				<div class="s1">
-					
-					<div class="s2">
-						
-						<?php foreach ( @$this->mcm->loaded_modules[ 'bottom_content' ] as $key => $module ) {
-							
-							echo @$module;
-							
-						} ?>
-						
-					</div>
-					
-				</div>
-				
-			</div>
-			
-			<?php } ?>
 			
 			<?php
 				
 				$footer_col_count = 0;
 				
-				if ( @$this->mcm->loaded_modules[ 'footer-left' ] ) $footer_col_count++;
-				if ( @$this->mcm->loaded_modules[ 'footer-center' ] ) $footer_col_count++;
-				if ( @$this->mcm->loaded_modules[ 'footer-right' ] ) $footer_col_count++;
+				if ( check_var( $this->mcm->loaded_modules[ 'footer_left' ] ) ) $footer_col_count++;
+				if ( check_var( $this->mcm->loaded_modules[ 'footer_center' ] ) ) $footer_col_count++;
+				if ( check_var( $this->mcm->loaded_modules[ 'footer_right' ] ) ) $footer_col_count++;
 				
 			?>
+			
+			<?php if ( check_var( $this->mcm->loaded_modules[ 'footer_top' ] ) ){ ?>
+			
+			<footer id="footer-top-block">
+				
+				<div class="s1">
+					
+					<div id="footer-module-top" class="footer-top">
+						
+						<div class="s1">
+							
+							<?php foreach ( $this->mcm->loaded_modules[ 'footer_top' ] as $key => $module ) { echo @$module; } ?>
+							
+						</div>
+						
+					</div>
+					
+				</div>
+				
+			</footer>
+			
+			<?php } ?>
+			
+			<?php if ( check_var( $this->mcm->loaded_modules[ 'footer_left' ] ) OR check_var( $this->mcm->loaded_modules[ 'footer_center' ] ) OR check_var( $this->mcm->loaded_modules[ 'footer_right' ] ) ){ ?>
 			
 			<footer id="footer-block" class="footer-<?= $footer_col_count; ?>-cols">
 				
 				<div class="s1">
 					
-					<?php if ( @$this->mcm->loaded_modules[ 'footer-left' ] ){ ?>
+					<?php if ( check_var( $this->mcm->loaded_modules[ 'footer_left' ] ) ){ ?>
 					
 					<div id="footer-module-left" class="footer-col">
 						
 						<div class="s1">
 							
-							<?php foreach ( @$this->mcm->loaded_modules[ 'footer-left' ] as $key => $module ) { echo @$module; } ?>
+							<?php foreach ( $this->mcm->loaded_modules[ 'footer_left' ] as $key => $module ) { echo @$module; } ?>
 							
 						</div>
 						
@@ -220,27 +316,27 @@
 					
 					}
 					
-					if ( @$this->mcm->loaded_modules[ 'footer-center' ] ){
-					
+					if ( check_var( $this->mcm->loaded_modules[ 'footer_center' ] ) ){
+
 					?><div id="footer-module-center" class="footer-col">
-						
+
 						<div class="s1">
-							
-							<?php foreach ( @$this->mcm->loaded_modules[ 'footer-center' ] as $key => $module ) { echo @$module; } ?>
-							
+
+							<?php foreach ( $this->mcm->loaded_modules[ 'footer_center' ] as $key => $module ) { echo @$module; } ?>
+
 						</div>
-						
+
 					</div><?php
-					
+
 					}
-					
-					if ( @$this->mcm->loaded_modules[ 'footer-right' ] ){
+
+					if ( check_var( $this->mcm->loaded_modules[ 'footer_right' ] ) ){
 					
 					?><div id="footer-module-right" class="footer-col">
 						
 						<div class="s1">
 							
-							<?php foreach ( @$this->mcm->loaded_modules[ 'footer-right' ] as $key => $module ) { echo @$module; } ?>
+							<?php foreach ( $this->mcm->loaded_modules[ 'footer_right' ] as $key => $module ) { echo @$module; } ?>
 							
 						</div>
 						
@@ -248,17 +344,11 @@
 					
 					<?php } ?>
 					
-					<?php /* ?>
-					<?= 'environment = <b>' . environment(); ?></b><br/>
-					current_component = <b> <?php var_dump($current_component); ?></b><br/>
-					<?= 'component_function = <b>' . $component_function; ?></b><br/>
-					<?= 'component_function_action = <b>' . $component_function_action; ?></b><br/>
-					<?= 'last_url = <b>'.get_last_url(); ?></b>
-					<?php */ ?>
-					
 				</div>
 				
 			</footer>
+			
+			<?php } ?>
 			
 		</div>
 		
