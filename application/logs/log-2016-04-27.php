9d8f79c95d65356e0b37a8a0f25176dcc15b47ad
franksouza183 <franksouza183@gmail.com>
2017-03-21 15:57:53 -0300

    massive update

--- application/logs/log-2016-04-27.php
+++
@@ -1,6 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
-
-ERROR - 2016-04-27 15:07:58 --> Severity: Notice  --> Undefined index: admin_hide_pagination_when_only_one_page /media/sites/openvizz/application/helpers/pagination_helper.php 20 --- File: core/Exceptions.php --- Line: 91
-ERROR - 2016-04-27 15:08:04 --> Severity: Notice  --> Undefined index: admin_hide_pagination_when_only_one_page /media/sites/openvizz/application/helpers/pagination_helper.php 20 --- File: core/Exceptions.php --- Line: 91
-ERROR - 2016-04-27 15:08:16 --> Severity: Notice  --> Undefined index: admin_hide_pagination_when_only_one_page /media/sites/openvizz/application/helpers/pagination_helper.php 20 --- File: core/Exceptions.php --- Line: 91
-ERROR - 2016-04-27 15:08:24 --> 404 Page Not Found --> submit_forms/sfm --- File: core/Exceptions.php --- Line: 112
